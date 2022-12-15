e4a8e5da16ca5311b38db8a72e48fbb52d0bc328
ivaylo91 <ipenev91@gmail.com>
2022-12-15 12:30:45 +0200

    Create add article function

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,24 @@
 import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
-  // This block will be executed once the page is loaded and ready
+    // This block will be executed once the page is loaded and ready
+    const button = document.querySelector(".button");
+    button.addEventListener("click", () => {
 
-  const button = document.querySelector(".button");
-  button.addEventListener("click", () => {
-    alert("💣");
-  });
+    });
+
 });
+
+let add_article = () => {
+    const articleTemplate = document.createElement('article');
+    articleTemplate.classList.add("message");
+    articleTemplate.innerHTML = "sample text sample text sample text sample text";
+    document.body.appendChild(articleTemplate);
+}
+document.body.addEventListener("click", () => {
+    let flag = 5;
+    while (flag !== 0) {
+        add_article();
+        flag--;
+    }
+})
